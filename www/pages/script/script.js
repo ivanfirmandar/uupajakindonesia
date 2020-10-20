de05533ae788450724dbf637bc9b69793a23498c
ivanfirmandar <ivanfirmandar@gmail.com>
2020-10-20 09:14:07 +0700

    Add Note Feature

--- www/pages/script/script.js
+++ www/pages/script/script.js
@@ -6,6 +6,7 @@ let keteranganTemplength;
 async function main() {
     let datas = await fetchData()
     setDatas(datas);
+    addNote()
     domHandler(datas);
 }
 
@@ -14,6 +15,7 @@ async function fetchData() {
 }
 
 function setDatas(datas) {
+    //Untuk Setiap Data :
     for (let i = 0; i < datas.length; i++) {
         let pasalIni = datas[i].getAttribute('data');
         let isiPasal = datas[i].innerHTML;
@@ -24,23 +26,16 @@ function setDatas(datas) {
 
 function setLink(isi, namapasal) {
     let pattern = "Pasal\\s+(\\w\\w\\w|\\w\\w|\\w)\\s+ayat\\s+\\((\\w\\w|\\w)\\)\\s+\\huruf\\s+\\w|Pasal\\s+(\\w\\w\\w|\\w\\w|\\w)\\s+ayat\\s+\\((\\w|\\w\\w)\\)|Pasal\\s+(\\w\\w\\w|\\w\\w|\\w)|Pasal\\s+(\\w\\w\\w|\\w\\w|\\w)|ayat\\s+\\((\\w|\\w\\w)\\)\\s+huruf\\s+\\w|ayat\\s+\\((\\w|\\w\\w)\\)|huruf\\s+\\w$";
-    // console.log(isi)
     let regexss = new RegExp(pattern, 'gi')
-    let regexss2 = new RegExp(pattern, '');
-    // console.log(namapasal)
-    namapasal = namapasal.split(" ")[1];
-    let matchedIsi = isi.match(regexss);
+    let regexss2 = new RegExp(pattern, '')
+    namapasal = namapasal.split(" ")[1]
+    let matchedIsi = isi.match(regexss)
     if (matchedIsi !== null) {
         matchedIsi.forEach(element => {
-            element = element.toUpperCase();
-            console.log("SEBELUM DIGANTI")
-            console.log(element)
+            element = element.toUpperCase()
             let regeee = new RegExp('\\s+', 'gim')
-            // console.log(element.match(regeee))
             element = element.replace(regeee, ' ')
-            console.log("SETELAH DIGANTI")
-            console.log(element)
-            isi = isi.replace(regexss2, `<span class='keterangan' data=${namapasal}>${element}</span>`);
+            isi = isi.replace(regexss2, `<span class='keterangan' data=${namapasal}>${element}</span>`)
         });
     } else {
         isi = isi;
@@ -83,7 +78,57 @@ async function findThings() {
 function refresh() {
     location.reload()
 }
+function addNote() {
+    let judulPasal = document.getElementsByClassName('pasal-nama')
+    for (let index = 0; index < judulPasal.length; index++) {
+        let namaPasal = judulPasal[index].innerHTML.replace(/\s+/g, " ")
+        let id = judulPasal[index].innerHTML.replace(/\s+/g, "")
+        judulPasal[index].innerHTML += `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-journal-text toolbar-icon tombol-note" fill="currentColor" onclick="showNote('${id}','${namaPasal}')"
+            xmlns="http://www.w3.org/2000/svg" data="${id}">
+            <path
+                d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
+            <path
+                d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
+            <path fill-rule="evenodd"
+                d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
+        </svg>`
 
+    }
+}
+function showNote(id, namaPasal) {
+    document.getElementsByClassName('note-container')[0].classList.remove('hide')
+    let uuName = document.getElementById('judulUU').innerHTML
+    document.getElementsByClassName('note-container')[0].innerHTML = `<div class="note">
+    <textarea id="noteText" placeholder="Belum ada Catatan">${showNoteText(id, uuName)}</textarea>
+     <div class="addNote">  
+    <span><button onclick="closeNote()">Tutup</button></span><span><button onclick="saveNote('${id}','${uuName}')" id="saveNoteButton">Tersimpan</button>    </span><h5 id="namaPasalNote">${namaPasal}</h5>
+    </div>
+    </div>
+   `
+    let noteText = document.getElementById('noteText')
+    noteText.addEventListener('keyup', () => {
+        document.getElementById('saveNoteButton').setAttribute('style', 'background-color : red')
+        document.getElementById('saveNoteButton').innerHTML = "Simpan"
+    })
+}
+function showNoteText(id, uuName) {
+    let note = localStorage.getItem(`${uuName} ${id}`)
+    if (note == null) {
+        return ""
+    } else {
+        return note;
+    }
+
+}
+function saveNote(id, uuName) {
+    let note = document.getElementById('noteText').value
+    localStorage.setItem(`${uuName} ${id}`, note);
+    document.getElementById('saveNoteButton').setAttribute('style', 'background-color : #1A3B50')
+    document.getElementById('saveNoteButton').innerHTML = "Tersimpan"
+}
+function closeNote() {
+    document.getElementsByClassName('note-container')[0].classList.add('hide')
+}
 function showKeterangan(datas) {
     let keterangan = document.getElementsByClassName("keterangan");
     keteranganPermLength = keterangan.length;
@@ -136,9 +181,6 @@ function keteranganParser(keterangan, attribute) {
         default:
             break;
     }
-    console.log(`Pasal : ${pasal}`);
-    console.log(`Ayat : ${ayat}`)
-    console.log(`Huruf : ${huruf}`)
     return encodeId(pasal, ayat, huruf, attribute);
 }
 
